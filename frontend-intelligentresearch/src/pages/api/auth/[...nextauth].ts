b22ea3341a2364bc2a254a3765c25bf3a3473d27
Gustav Uwe <gustavuwe.123@gmail.com>
2024-07-27 18:20:19 -0300

    add frontend system with nextjs and tailwindcss

---
+++ frontend-intelligentresearch/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/../../backend-intelligentResearch/src/lib/prisma";
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
