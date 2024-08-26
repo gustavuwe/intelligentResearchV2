d545ac81479057f39e8a9fc48e0add864dd84044
Gustav Uwe <gustavuwe.123@gmail.com>
2024-08-26 13:47:58 -0300

    fix: fix loading component

--- frontend-intelligentresearchv2-updated/src/app/home/page.tsx
+++ frontend-intelligentresearchv2-updated/src/app/home/page.tsx
@@ -912,7 +912,7 @@ export default function HomePage() {
   );
 }
 
-export const Loading = () => {
+const Loading = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
--- frontend-intelligentresearchv2-updated/src/app/reportar/page.tsx
+++ frontend-intelligentresearchv2-updated/src/app/reportar/page.tsx
@@ -7,7 +7,6 @@ import axios from "axios";
 import { ArrowLeft, Check } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Loading } from "../home/page";
 import Link from "next/link";
 
 const GetInTouch = () => {
@@ -125,6 +124,14 @@ const GetInTouch = () => {
       </div>
     </section>
     </>
+  );
+};
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
   );
 };
 
