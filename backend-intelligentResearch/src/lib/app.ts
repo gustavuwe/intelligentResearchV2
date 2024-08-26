7113ce7b9310ea428aa84bd7be085ad3d6ed0c2b
Gustav Uwe <gustavuwe.123@gmail.com>
2024-08-26 17:51:52 -0300

    fix: fixed cors

--- backend-intelligentResearch/src/lib/app.ts
+++ backend-intelligentResearch/src/lib/app.ts
@@ -18,7 +18,7 @@ export const app = Fastify()
 //     expiresIn: '10m',
 //   },
 // }
-app.register(cors, { origin: false })
+app.register(cors, { origin: '*' })
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
